ca83463de828bb8a85af8e4adcdf004fc1767a25
FinleyNeilson <finleyjohnneilson@gmail.com>
2025-08-23 10:37:05 +1200

    Textures

--- Glitter/Shaders/fragmentShader.frag
+++ Glitter/Shaders/fragmentShader.frag
@@ -1,8 +1,19 @@
 #version 330 core
 layout(location = 0) out vec4 FragColor;
-in vec3 ourColor;
+
+// in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform vec4 ourColor;
                                          
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+  // Sample the texture
+  // vec4 texColor = texture(texture2, TexCoord);
+  vec4 texColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+
+  // Modulate it by ourColor (convert to vec4 with alpha 1.0)
+  FragColor = texColor * ourColor;
 }
--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,6 +1,7 @@
 // Local Headers
 #include "Shader.h"
 #include "glitter.hpp"
+#include "stb_image.h"
 
 // System Headers
 #include <GLFW/glfw3.h>
@@ -12,6 +13,7 @@
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(const std::string &path);
 
 int main(int argc, char *argv[]) {
   (void)argc, (void)argv;
@@ -42,13 +44,20 @@ int main(int argc, char *argv[]) {
   Shader shader("../Glitter/Shaders/vertexShader.vert",
                 "../Glitter/Shaders/fragmentShader.frag");
 
-  float vertices[] = {// positions         // colors
-                      0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, -0.5f, -0.5f, 0.0f,
-                      0.0f, 1.0f,  0.0f, 0.0f, 0.5f, 0.0f, 0.0f,  0.0f,  1.0f};
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
 
   unsigned int indices[] = {
-      0, 1, 2,  // first triangle
+      0, 1, 2, 2, 3, 0,
   };
+
+  unsigned int texture1 = loadTexture("container.jpg");
+  unsigned int texture2 = loadTexture("awesomeface.png");
 
   unsigned int VBO;
   glGenBuffers(1, &VBO);
@@ -70,11 +79,16 @@ int main(int argc, char *argv[]) {
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)0); // position
   glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float))); // color
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float))); // texCoords
+  glEnableVertexAttribArray(2);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
   // ..:: Rendering Loop :: ..
@@ -89,6 +103,8 @@ int main(int argc, char *argv[]) {
     float greenValue = (sin(time) / 2.0f) + 0.5f;
 
     shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
     shader.setMat4("uModel", model);
     shader.setVec4("ourColor", glm::vec4(0.0f, greenValue, 0.0f, 1.0f));
 
@@ -96,9 +112,13 @@ int main(int argc, char *argv[]) {
     glClearColor(0.25f, 0.25f, 0.75f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // Flip Buffers and Draw
     glfwSwapBuffers(mWindow);
@@ -124,4 +144,43 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   // make sure the viewport matches the new window dimensions; note that width
   // and height will be significantly larger than specified on retina displays.
   glViewport(0, 0, width, height);
+}
+
+unsigned int loadTexture(const std::string &path) {
+  unsigned int textureID;
+
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+
+  // Set the texture wrapping/filtering options
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Load image
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true); // optional: flip vertically
+  unsigned char *data =
+      stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+  if (data) {
+    GLenum format = GL_RGB;
+    if (nrChannels == 1)
+      format = GL_RED;
+    else if (nrChannels == 3)
+      format = GL_RGB;
+    else if (nrChannels == 4)
+      format = GL_RGBA;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture: " << path << std::endl;
+  }
+  stbi_image_free(data);
+
+  return textureID;
 }
